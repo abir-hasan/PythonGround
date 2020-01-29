c174570d3298ee05121ecc9ab2bfd84f150d8a6d
Abir <abirhasan89@gmail.com>
2020-01-29 23:37:04 +0600

    Section 2 practice - with examples

---
+++ scratch_files/scratch_section_2.py
@@ -0,0 +1,74 @@
+############### Section 2 Language Overview ################
+
+# import platform
+#
+# version = platform.python_version()
+# print('this is python version: {}'.format(version))
+
+# Example with pre-formatted and format function
+# name = "Abir"
+# # pre-formatted f
+# print(f"Hello worlds {name}")
+# # with format function
+# print("Hello world{}".format(name))
+
+# Example with main function
+# def message():
+#     print("Test")
+#
+# if __name__ == "__main__" : message()
+
+# Blocks and Scope
+# x = 45
+# y = 32
+#
+# if x > y:
+#     z = 55
+#     print("x is greater than y")
+#
+# print("z doesn't fall bound to the if scope and value: {}".format(z))
+
+# Conditionals [Python doesn't have switch-case]
+# x = 10
+# y = 10
+#
+# if x > y:
+#     print("x is greater than y")
+# elif x < y:
+#     print("x is less than y")
+# else:
+#     print("x and y are equal")
+
+# While Loop example
+# words = ['one', 'two', 'three', 'four', 'five']
+# n = 0
+# while n < 5:
+#     print("{}".format(words[n]))
+#     n += 1
+
+# For Loop example
+# words = ['one', 'two', 'three', 'four', 'five']
+# for i in words:
+#     print(i)
+
+# Function example with default value
+# def function(n=5):
+#     print(n)
+# function()
+# function(100)
+
+# Example of a Class in Python
+# class Duck:
+#     sound = "Bla"
+#     color = "White"
+#
+#     def quack(self):
+#         print(self.sound)
+#
+#     def look(self):
+#         print(self.color)
+#
+#
+# duck = Duck()
+# duck.look()
+# duck.quack()
