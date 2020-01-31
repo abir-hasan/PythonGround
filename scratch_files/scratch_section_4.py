283df55db6ad0f72b6f3ec0c8ebd20c7264258fc
Abir <abirhasan89@gmail.com>
2020-01-31 23:54:57 +0600

    Section 4 practice - with examples
    - ternary operation

---
+++ scratch_files/scratch_section_4.py
@@ -0,0 +1,11 @@
+############### Section 4: Conditionals ################
+
+# Ternary operation
+hungry = True
+x = "Feed the dog" if hungry else "Don't give him food"
+
+can_swim = False
+y = "Get into the pool" if can_swim else "Don't go near the pool"
+
+print(x)
+print(y)
