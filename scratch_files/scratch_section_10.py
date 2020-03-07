66f70cfb329eba175c4d3b98e588eb7e5bad1de9
Abir <abirhasan89@gmail.com>
2020-03-07 18:51:05 +0600

    Section 10 practice - with examples
    - Exception handling and Error reporting

---
+++ scratch_files/scratch_section_10.py
@@ -0,0 +1,37 @@
+############### Section 10: Exceptions ################
+
+# With a Generator example
+def inclusive_range(*args):
+    numargs = len(args)
+    start = 0
+    step = 1
+
+    # initialize parameters
+    if numargs < 1:
+        raise TypeError(f'expected at least 1 argument, got {numargs}')
+    elif numargs == 1:
+        stop = args[0]
+    elif numargs == 2:
+        (start, stop) = args
+    elif numargs == 3:
+        (start, stop, step) = args
+    else:
+        raise TypeError(f'expected at most 3 arguments, got {numargs}')
+
+    # generator
+    i = start
+    while i <= stop:
+        yield i
+        i += step
+
+
+def main():
+    try:
+        for i in inclusive_range():
+            print(i, end=' ', flush=True)
+        print()
+    except TypeError as e:
+        print(f"range error: {e}")
+
+
+if __name__ == '__main__': main()
