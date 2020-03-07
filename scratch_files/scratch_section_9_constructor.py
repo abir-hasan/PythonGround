d93538097b3c8a7507d24458c2974e204e688b56
Abir <abirhasan89@gmail.com>
2020-03-07 17:57:19 +0600

    Section 9 practice - with examples
    - Class and constructor

---
+++ scratch_files/scratch_section_9_constructor.py
@@ -0,0 +1,38 @@
+############### Section 9: Classes ################
+
+class Animal:
+    # def __init__(self, type, name, sound):
+    #     self._type = type
+    #     self._name = name
+    #     self._sound = sound
+
+    # Doesn't support multiple constructor  like Java
+    def __init__(self, **kwargs):
+        self._type = kwargs["type"] if "type" in kwargs else "Animal"
+        self._name = kwargs["name"] if "name" in kwargs else "Name"
+        self._sound = kwargs["sound"] if "sound" in kwargs else "Sound"
+
+    def type(self):
+        return self._type
+
+    def name(self):
+        return self._name
+
+    def sound(self):
+        return self._sound
+
+def print_animal(o):
+    if not isinstance(o, Animal):
+        raise TypeError('print_animal(): requires an Animal')
+    print('The {} is named "{}" and says "{}".'.format(o.type(), o.name(), o.sound()))
+
+
+def main():
+    a0 = Animal(type='kitten', name='fluffy', sound='rwar')
+    a1 = Animal(type='duck', name='donald', sound='quack')
+    print_animal(a0)
+    print_animal(a1)
+    print_animal(Animal(type='velociraptor', name='veronica', sound='hello'))
+    print_animal(Animal())
+
+if __name__ == '__main__': main()
