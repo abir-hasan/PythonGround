2b9555cb4fb505bd67aa697dc4ddac68eaf91de4
Abir Hasan <abirhasan89@gmail.com>
2020-11-21 21:53:47 +0600

    removed unused lines of code and comment added

--- web_science/assignment_2/task_2.py
+++ web_science/assignment_2/task_2.py
@@ -1,7 +1,7 @@
 initial_bit_size = 10
 formatted_bit_size = initial_bit_size - 2
 perfect_sum = "11111111"  # same size bit as 'formatted_bit_size'
-main_overflow_bit = "0b00000001"  # same size bit as 'initial_bit_size'
+overflow_bit = "0b00000001"  # same size bit as 'initial_bit_size'
 initial_sum = "0b00000000"  # same size bit as 'initial_bit_size'
 
 
@@ -22,7 +22,7 @@ def calculate_checksum(message):
     return cs
 
 
-# Method to Validate Data
+# Method to Validate Message against checksum
 def validate_data(message, check_sum):
     message.append(check_sum)
     sum = initial_sum
@@ -38,9 +38,7 @@ def validate_data(message, check_sum):
 
 
 def print_check_sum(sum):
-    # value = sum[2:]  # Slicing the variable
     check_sum = ""
-    # print(f"print_check_sum {len(sum)}")
     for i in sum:  # Flipping values to get 1's compliment
         if i == "0":
             check_sum += "1"
@@ -50,17 +48,15 @@ def print_check_sum(sum):
     return check_sum
 
 
+# Get the wrapped value
+# If there is an overflow then
+# Add it to the sum and return
 def get_sum_wrapped(sum):
     x = len(sum)
-    # print(f"get_sum_wrapped() len {x}")
     if x > initial_bit_size:
         y = sum[2:]  # removing '0b'
-        # print(f"y1    {y}")
         y = y[1:]  # slicing the number except the overflow bit
-        # print(f"y2    {y}")
-        c = main_overflow_bit  # overflow bit
-        z = bin(int(y, 2) + int(c, 2))
-        # print(f"z     {z}")
+        z = bin(int(y, 2) + int(overflow_bit, 2))  # Add overflow bit to the sum
         return z
     else:
         return sum
@@ -75,7 +71,6 @@ def print_variables(value, count):
 
 
 def get_formatted_bits(value):
-    # print(f"get_formatted_bits() {value}")
     value = value[2:]  # Slicing the variable
     x = len(value)
     if x == formatted_bit_size:
@@ -90,8 +85,8 @@ def get_formatted_bits(value):
 
 
 if __name__ == "__main__":
-    # a = [0b1010101100110101, 0b1100001111010001, 0b0101001000110001, 0b0011000011101101]
-    # cs_x = int(calculate_checksum(a), 2)
+    # a = [0b1010101100110101, 0b1100001111010001, 0b0101001000110001, 0b0011000011101101] # Change Upper values
+
     x = [0b01101001, 0b00100000, 0b01101100, 0b01101111]
     y = [0b01110110, 0b01100101, 0b00100000, 0b01110111]
     z = [0b01100101, 0b01100010, 0b00100000, 0b01110011]
