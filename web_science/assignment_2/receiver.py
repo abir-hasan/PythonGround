44a18f80ddb2c443128444037dea75c3aaed83ea
Abir Hasan <abirhasan89@gmail.com>
2020-11-23 22:33:49 +0600

    WS-Assignment-2 [Task 2.2 Formatted and commented receiver.py and sender.py]

--- web_science/assignment_2/receiver.py
+++ web_science/assignment_2/receiver.py
@@ -1,3 +1,20 @@
+# Web Science Assignment - 2
+# Team - Mike
+# Task 2 - Python Programming
+# 2.2 TCP Client Server
+# In this task you will simulate the checksum operation. You will write a sender (sender.py)
+# and a receiver (receiver.py). The communication between them will be with IPv4 and TCP.
+# 1. sender.py generates random messages as shown Table 1. When it gets four segments
+#    of messages, it calculates the checksum of a message with the method you will
+#    implement for Assignment 2.1 and send them to the receiver.
+# 2. The receiver.py performs checksum validation with the method you will implement
+#    for Assignment 2.1 and sends the result of the validation to the sender.
+# 3. When you execute python receiver.py from the console, you should print out the
+#    flow of receiver similarly.
+# 4. When you execute python sender.py from the console, you should print out the
+#    flow of sender similarly:
+
+
 import pickle
 import socket
 
@@ -8,14 +25,14 @@ CON_PORT = 10000
 
 
 def start_server():
-    # Create a TCP/IP socket
+    # Creating a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Bind the socket to the port
+    # Binding the socket to the port
     server_address = (LOCAL_HOST, CON_PORT)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Using this to reuse same 'server_address'
     sock.bind(server_address)
-    # Listen for incoming connections
+    # Listen for incoming connection
     sock.listen(1)
 
     while True:
@@ -23,17 +40,17 @@ def start_server():
         # print('waiting for a connection')
         connection, client_address = sock.accept()
         try:
-            in_con = client_address[0]
-            in_port = client_address[1]
+            in_con = client_address[0]  # Incoming Connection Address
+            in_port = client_address[1]  # Incoming Connection Port
             in_server = f"{in_con}:{in_port}"
             print(f'connection from {in_server}')
 
-            # Receive the data in small chunks and retransmit it
+            # Receive message in small chunks
             while True:
                 data = connection.recv(48)
                 data = pickle.loads(data)  # Using pickle to get 'list' out of received payload
-                messages = data[0:4]
-                check_sum = data[4]
+                messages = data[0:4]  # Getting first 4 elements which are messages
+                check_sum = data[4]  # Last element in the list is the checksum
                 print(f"Got the following message {messages} and {check_sum} as checksum from the sender")
                 is_valid = validate_data(messages, check_sum)  # Validate message against checksum
                 if data:
@@ -45,7 +62,7 @@ def start_server():
                     else:
                         final_message = "Corrupted data"
                         connection.sendall(bytes(final_message, 'utf-8'))
-                    connection.sendall(bytes("exit", 'utf-8'))  # Sending signal to close
+                    connection.sendall(bytes("exit", 'utf-8'))  # Sending signal to close client connection
                 else:
                     print(f'no more data from {client_address}')
                     break
--- web_science/assignment_2/sender.py
+++ web_science/assignment_2/sender.py
@@ -1,3 +1,19 @@
+# Web Science Assignment - 2
+# Team - Mike
+# Task 2 - Python Programming
+# 2.2 TCP Client Server
+# In this task you will simulate the checksum operation. You will write a sender (sender.py)
+# and a receiver (receiver.py). The communication between them will be with IPv4 and TCP.
+# 1. sender.py generates random messages as shown Table 1. When it gets four segments
+#    of messages, it calculates the checksum of a message with the method you will
+#    implement for Assignment 2.1 and send them to the receiver.
+# 2. The receiver.py performs checksum validation with the method you will implement
+#    for Assignment 2.1 and sends the result of the validation to the sender.
+# 3. When you execute python receiver.py from the console, you should print out the
+#    flow of receiver similarly.
+# 4. When you execute python sender.py from the console, you should print out the
+#    flow of sender similarly:
+
 import pickle
 import random
 import socket
@@ -14,7 +30,7 @@ Z = [0b01100101, 0b01100010, 0b00100000, 0b01110011]
 
 
 def start_client():
-    # Create a TCP/IP socket
+    # TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Connect the socket to the port where the server is listening
@@ -22,10 +38,11 @@ def start_client():
     print(f'connected to tmps-{LOCAL_HOST}:{CON_PORT}')
     sock.connect(server_address)
 
-    # Generating four 8-bit values
+    # Generating four 8-bit random massages
     message_list = random.sample(range(0, 256), 4)
-
+    # Calculating checksum to send to the server, along side the messages
     check_sum_int = int(calculate_checksum(message_list), 2)
+    # Appending the checksum with the message list to send data at one shot
     message_list.append(check_sum_int)
     try:
         # Send Data to Server
@@ -33,7 +50,7 @@ def start_client():
     finally:
         while True:
             ack = sock.recv(300)
-            #ack = ack.decode(ENCODING)
+            # ack = ack.decode(ENCODING)
             if ack == "exit".encode(ENCODING):
                 break
             else:
@@ -42,6 +59,7 @@ def start_client():
     sock.close()
 
 
+# Method to send data to the Server
 def send_data(sock, message):
     # Send data
     data = pickle.dumps(message)  # Using pickle to send 'list' as payload
